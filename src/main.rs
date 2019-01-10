72a00c361bc19c0b701a977f646db333b0c9e2ea
William Shiao <willshiao@gmail.com>
2019-01-10 12:07:41 -0800

    Add support for iterated MD5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate crypto;
+
+use hex;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
 struct WordGenerator {
     counts: Vec<usize>,
     vocab: Vec<char>,
@@ -51,14 +57,50 @@ impl Iterator for WordGenerator {
     }
 }
 
+fn create_hash (data: &[u8], iterations: usize) -> [u8; 16] {
+    let mut hasher = Md5::new();
+    let mut output = [0; 16];
+    
+
+    for i in 0..iterations {
+        if i == 0 {
+            hasher.input(data);
+        } else {
+            hasher.input(&output);
+        }
+        hasher.result(&mut output);
+        hasher.reset();
+    }
+    return output;
+}
+
 fn main() {
     let vocab = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let mut cnt = 0;
+    let target = hex::decode("62aac4b5c341119bb71bd8e378bc7b8a").expect("Ooops");
 
-    let counter = WordGenerator::new(6, vocab);
-    for _i in counter {
+    // let output = [0; 16];
+    // let mut hasher = Md5::new();
+    let counter = WordGenerator::new(5, vocab);
+
+    for x in counter {
+        
+        // hasher.input(x.as_bytes());
+
+        // hasher.result(&mut output);
+        if create_hash(x.as_bytes(), 1000) == &target[..] {
+            println!("Done w/ {}", x);
+        }
+        // if format!("{:x}", md5::compute(&x)) == target {
+        //     println!("Done w/ {}", x);
+        // }
+        // hasher.reset();  
         cnt += 1;
     }
 
     println!("Total count: {}", cnt);
+    // println!("Last: {}", last);
 }
+
+
+
