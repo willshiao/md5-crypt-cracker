d5e4a085cae142d7e2e514095c44a7eb588f1788
Calvin <tacalvin123@gmail.com>
2019-01-11 14:28:50 -0800

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,52 @@
-struct WordGenerator {
-    counts: Vec<usize>,
-    vocab: Vec<char>,
-    is_start: bool
-}
+use std::thread;
+use crossbeam_channel::bounded;
 
-impl WordGenerator {
-    fn convert_word(&self) -> String {
-        self.counts.iter().map(|x| self.vocab[*x]).collect()
-    }
-
-    fn new(target_len : usize, vocab : Vec<char>) -> WordGenerator {
-        WordGenerator {
-            counts: vec![0; target_len],
-            vocab: vocab,
-            is_start: true
-        }
-    }
-}
-
-impl Iterator for WordGenerator {
-    type Item = String;
-
-    fn next(&mut self) -> Option<String> {
-        if self.is_start {
-            self.is_start = false;
-            return Some(self.convert_word());
-        }
-
-        let len = self.counts.len();
-        let mut all_full = false;
-
-        for i in 1..len+1 {
-            if self.counts[len - i] < self.vocab.len() - 1 {
-                self.counts[len - i] += 1;
-                break;
-            } else if i == len {
-                all_full = true;
-            } else {
-                for j in 1..i + 1 {
-                    self.counts[len - j] = 0;
-                }
+mod doge;
+// use threadpool::ThreadPool;
+// use spmc;
+fn work(s: crossbeam_channel::Sender<Option<String>> , r: crossbeam_channel::Receiver<Option<String>> , n_workers: &u32, counter: &mut doge::WordGenerator){
+    for n in 0..*n_workers{
+        let rx = r.clone();
+        thread::spawn(move || {
+            let mut _c = 0;
+            loop{
+                match rx.recv().unwrap(){
+                    Some(i) =>{
+                        _c += 1;
+                        continue;
+                    },
+                    None => {
+                        println!("Worker {} recieved {}", n, _c);
+                        break;
+                    },
+                } 
+            
             }
-        }
+        });
+    }
 
-        if all_full {
-            None
-        } else {
-            Some(self.convert_word())
-        }
+    for i in counter{
+        s.send(Some(i.clone())).unwrap_or_else(|err|{
+            println!("Channel has died");
+        });
+    }
+    for i in 0..*n_workers{
+        s.send(None).unwrap_or_else(|err|{
+            println!("Failed to send terminating signal to threads: {}", err);
+        });
     }
 }
 
 fn main() {
+    let user_creds = doge::UserCreds::parse_user_input();
+
+
     let vocab = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let mut cnt = 0;
-
-    let counter = WordGenerator::new(6, vocab);
-    for _i in counter {
-        cnt += 1;
-    }
+    // let n_workers = 7;
+    let mut counter = doge::WordGenerator::new(6, vocab);
+    let (s, r) = bounded(user_creds.n_workers as usize);
+    work(s, r, &user_creds.n_workers, &mut counter);
 
     println!("Total count: {}", cnt);
 }
