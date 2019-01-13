178fc90ca52056c2f6847324c7d9fe6030feac21
William Shiao <willshiao@gmail.com>
2019-01-12 23:15:02 -0800

    Add indicatif support

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,92 @@
-extern crate crypto;
 extern crate base64;
+extern crate crypto;
 
-use hex;
-use crypto::md5::Md5;
-use crypto::digest::Digest;
-use std::thread;
 use crossbeam_channel::bounded;
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+use hex;
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use std::thread;
 
 mod doge;
 // use threadpool::ThreadPool;
 // use spmc;
 
-const BYTE_ORDERINGS: [usize; 16] = [11,4,10,5,3,9,15,2,8,14,1,7,13,0,6,12];
+const BYTE_ORDERINGS: [usize; 16] = [11, 4, 10, 5, 3, 9, 15, 2, 8, 14, 1, 7, 13, 0, 6, 12];
 
-fn work(s: crossbeam_channel::Sender<Option<String>> , r: crossbeam_channel::Receiver<Option<String>> , n_workers: &u32, counter: &mut doge::WordGenerator){
-    for n in 0..*n_workers{
+fn work(
+    s: crossbeam_channel::Sender<Option<String>>,
+    r: crossbeam_channel::Receiver<Option<String>>,
+    n_workers: &u32,
+    counter: &mut doge::WordGenerator,
+) {
+    let bar = ProgressBar::new(counter.get_size());
+    bar.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {bar:50.cyan/blue} {pos:>7}/{len:7} {eta_precise} {msg}"));
+
+    for n in 0..*n_workers {
         let rx = r.clone();
         thread::spawn(move || {
             let mut _c = 0;
-            loop{
-                match rx.recv().unwrap(){
-                    Some(i) =>{
+            loop {
+                match rx.recv().unwrap() {
+                    Some(i) => {
                         _c += 1;
                         continue;
-                    },
+                    }
                     None => {
                         println!("Worker {} recieved {}", n, _c);
                         break;
-                    },
-                } 
-            
+                    }
+                }
             }
         });
-
     }
 
-    for i in counter{
-        s.send(Some(i.clone())).unwrap_or_else(|err|{
+    let mut sent_cnt : u32 = 0;
+    for i in counter {
+        s.send(Some(i.clone())).unwrap_or_else(|err| {
             println!("Channel has died");
         });
+        bar.inc(1);
+        sent_cnt += 1;
+        if sent_cnt % 25000 == 0 {
+            bar.inc(25000);
+        }
     }
-    for i in 0..*n_workers{
-        s.send(None).unwrap_or_else(|err|{
+    for i in 0..*n_workers {
+        s.send(None).unwrap_or_else(|err| {
             println!("Failed to send terminating signal to threads: {}", err);
         });
     }
+    bar.finish();
 }
 
 struct Md5Crypt {
     password: Vec<u8>,
     salt: Vec<u8>,
-    magic: Vec<u8>
+    magic: Vec<u8>,
 }
 
 impl Md5Crypt {
-    fn new (password: &String, salt: &String) -> Md5Crypt {
+    fn new(password: &String, salt: &String) -> Md5Crypt {
         Md5Crypt {
             password: password.as_bytes().to_vec(),
             salt: salt.as_bytes().to_vec(),
-            magic: "$1$".as_bytes().to_vec()
+            magic: "$1$".as_bytes().to_vec(),
         }
     }
 
-    fn alternate_sum (&self) -> [u8; 16] {
+    fn alternate_sum(&self) -> [u8; 16] {
         let mut all = self.password.clone();
         all.extend(&self.salt);
         all.extend(&self.password);
-        
+
         create_hash(&all[..], 1)
     }
 
-    fn intermediate_sum (&self) -> [u8; 16] {
+    fn intermediate_sum(&self) -> [u8; 16] {
         let mut output: [u8; 16] = [0; 16];
         let mut hasher = Md5::new();
 
@@ -107,7 +123,7 @@ impl Md5Crypt {
         return output;
     }
 
-    fn hash (&self) -> [u8; 16] {
+    fn hash(&self) -> [u8; 16] {
         let i0 = self.intermediate_sum();
         println!("Intermediate sum: {}", hex::encode(&i0));
         let mut last_i = i0;
@@ -146,10 +162,10 @@ impl Md5Crypt {
             debug_vec.clear();
         }
 
-        return Md5Crypt::reorder_bytes(&last_i)
+        return Md5Crypt::reorder_bytes(&last_i);
     }
 
-    fn reorder_bytes (original: &[u8]) -> [u8; 16] {
+    fn reorder_bytes(original: &[u8]) -> [u8; 16] {
         let mut output: [u8; 16] = [0; 16];
         for i in 0..BYTE_ORDERINGS.len() {
             output[BYTE_ORDERINGS.len() - i - 1] = original[BYTE_ORDERINGS[i]];
@@ -158,8 +174,8 @@ impl Md5Crypt {
     }
 }
 
-fn create_hash (data: &[u8], iterations: usize) -> [u8; 16] {
-    let mut output : [u8; 16] = [0; 16];
+fn create_hash(data: &[u8], iterations: usize) -> [u8; 16] {
+    let mut output: [u8; 16] = [0; 16];
     let mut hasher = Md5::new();
 
     for i in 0..iterations {
@@ -177,33 +193,35 @@ fn create_hash (data: &[u8], iterations: usize) -> [u8; 16] {
 }
 
 fn main() {
-    let vocab = vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    // let user_creds = doge::UserCreds::parse_user_input();
+    let vocab = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+    let user_creds = doge::UserCreds::parse_user_input();
 
     // let n_workers = 7;
-    // let mut counter = doge::WordGenerator::new(6, vocab);
-    // let (s, r) = bounded(user_creds.n_workers as usize);
-    // work(s, r, &user_creds.n_workers, &mut counter);
+    let mut counter = doge::WordGenerator::new(6, vocab);
+    let (s, r) = bounded(user_creds.n_workers as usize);
+    work(s, r, &user_creds.n_workers, &mut counter);
 
     // let gen = doge::WordGenerator::new();
-    let pass = String::from("a");
-    let salt = String::from("bc");
+    // let pass = String::from("a");
+    // let salt = String::from("bc");
 
-    let crypt = Md5Crypt::new(&pass, &salt);
-    let res = crypt.hash();
-    let output = base64::encode_config(&res, base64::CRYPT);
-    println!("Output: {}", output);
-    println!("Output (hex): {}", hex::encode(&res));
+    // let crypt = Md5Crypt::new(&pass, &salt);
+    // let res = crypt.hash();
+    // let output = base64::encode_config(&res, base64::CRYPT);
+    // println!("Output: {}", output);
+    // println!("Output (hex): {}", hex::encode(&res));
 
-    let input = base64::decode_config("HgqpXhm.E0eACNRZkZJa", base64::CRYPT);
+    // let input = base64::decode_config("HgqpXhm.E0eACNRZkZJa", base64::CRYPT);
 
-    match input {
-        Ok(input) => println!("Target: {}", hex::encode(input)),
-        Err(err) => {
-            panic!("There was an error: {:?}", err)
-        },
-    };
-    
+    // match input {
+    //     Ok(input) => println!("Target: {}", hex::encode(input)),
+    //     Err(err) => {
+    //         panic!("There was an error: {:?}", err)
+    //     },
+    // };
 
     // println!("Total count: {}", cnt);
     // println!("Last: {}", last);
